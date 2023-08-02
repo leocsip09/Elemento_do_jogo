c1c1e5f01c5dd0ff181690ac920b9011f648f9d9
leocsip09 <131828922+leocsip09@users.noreply.github.com>
2023-08-02 06:21:49 -0500

    Update NivelNivel1.h

--- NivelNivel1.h
+++ NivelNivel1.h
@@ -7,23 +7,23 @@
 class NivelNivel1 : public Nivel {
 private:
     sf::RenderWindow nivel1Ventana; // Nueva ventana para el nivel 1
-    bool nivel1Activo; // Variable para controlar si la nueva ventana est· activa
+    bool nivel1Activo; // Variable para controlar si la nueva ventana est√° activa
     sf::Texture* imagen;
     sf::Sprite* fondo;
-    bool presionado;
-    sf::RectangleShape* cerrarVentana;
+ 
+    sf::RectangleShape* cerrarNivel1;
     sf::Vector2i pos_mouse;
     sf::Vector2f mouse_coord;
 public:
     // Constructor del nivel 1
     NivelNivel1();
 
-    // Sobrescribe los mÈtodos de la clase base
+    // Sobrescribe los m√©todos de la clase base
     void inicializar() override;
     void actualizar(sf::RenderWindow& ventana) override;
     void dibujar(sf::RenderWindow& ventana) override;
 
-    // MÈtodo para verificar si la nueva ventana del nivel 1 est· activa
+    // M√©todo para verificar si la nueva ventana del nivel 1 est√° activa
     bool estaActivo() const override;
 
     ~NivelNivel1();
