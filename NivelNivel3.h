2818cb279e806e2baee4093fae7c9189577149ff
leocsip09 <131828922+leocsip09@users.noreply.github.com>
2023-08-02 06:23:46 -0500

    Update NivelNivel3.h

--- NivelNivel3.h
+++ NivelNivel3.h
@@ -7,18 +7,25 @@
 class NivelNivel3 : public Nivel {
 private:
     sf::RenderWindow nivel3Ventana; // Nueva ventana para el nivel 3
-    bool nivel3Activo; // Variable para controlar si la nueva ventana est· activa
+    bool nivel3Activo; // Variable para controlar si la nueva ventana est√° activa
+    sf::Texture* imagen;
+    sf::Sprite* fondo;
 
+    sf::RectangleShape* cerrarNivel3;
+    sf::Vector2i pos_mouse;
+    sf::Vector2f mouse_coord;
 public:
     // Constructor del nivel 3
     NivelNivel3();
 
-    // Sobrescribe los mÈtodos de la clase base
+    // Sobrescribe los m√©todos de la clase base
     void inicializar() override;
     void actualizar(sf::RenderWindow& ventana) override;
     void dibujar(sf::RenderWindow& ventana) override;
 
-    // MÈtodo para verificar si la nueva ventana del nivel 3 est· activa
+    // M√©todo para verificar si la nueva ventana del nivel 3 est√° activa
     bool estaActivo() const override;
+
+    ~NivelNivel3();
 };
 
