4e1e3c63bcbe54dec2e81860607df2a047b38c9e
leocsip09 <131828922+leocsip09@users.noreply.github.com>
2023-08-02 06:21:06 -0500

    Update MultitonNivel.h

--- MultitonNivel.h
+++ MultitonNivel.h
@@ -2,13 +2,14 @@
 
 #include "Nivel.h"
 #include <unordered_map>
+#include <memory>
 
 class MultitonNivel {
 private:
-    // Contenedor para almacenar los punteros a las instancias de los niveles
-    static std::unordered_map<int, Nivel*> niveles;
+    // Contenedor para almacenar los smart pointers a las instancias de los niveles
+    static std::unordered_map<int, std::unique_ptr<Nivel>> niveles;
 
 public:
-    // Método para obtener una instancia del nivel según su número
+    // MÃ©todo para obtener una instancia del nivel segÃºn su nÃºmero
     static Nivel* getNivel(int numero);
 };
