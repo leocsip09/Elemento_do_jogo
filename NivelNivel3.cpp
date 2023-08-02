f711f155d793149900f205db33d65ec293516ea7
leocsip09 <131828922+leocsip09@users.noreply.github.com>
2023-08-02 06:23:24 -0500

    Update NivelNivel3.cpp

--- NivelNivel3.cpp
+++ NivelNivel3.cpp
@@ -1,17 +1,22 @@
 #include "NivelNivel3.h"
 #include <iostream>
 
-NivelNivel3::NivelNivel3() : Nivel(), nivel3Ventana(sf::VideoMode(1280, 720), "Nivel 3"), nivel3Activo(true) {}
-
-
-void NivelNivel3::inicializar() {
-    // L骻ica de inicializaci髇 para el nivel 3
+NivelNivel3::NivelNivel3() : Nivel(), nivel3Ventana(sf::VideoMode(1280, 720), "Nivel 3"), nivel3Activo(true) {
+    imagen = new sf::Texture();
+    fondo = new sf::Sprite();
+    cerrarNivel3 = new sf::RectangleShape();
     std::cout << "Se inializo el nivel 3 desde multiton" << std::endl;
 }
 
+
+void NivelNivel3::inicializar() {
+    // L贸gica de inicializaci贸n para el nivel 3
+
+}
+
 void NivelNivel3::actualizar(sf::RenderWindow& ventana) {
-    // L骻ica de actualizaci髇 para el nivel 3
-    std::cout << "Se esta actualizando el nivel 3" << std::endl;
+    // L贸gica de actualizaci贸n para el nivel 3
+
     if (nivel3Activo) {
         sf::Event event;
         while (nivel3Ventana.pollEvent(event)) {
@@ -20,19 +25,45 @@ void NivelNivel3::actualizar(sf::RenderWindow& ventana) {
                 nivel3Activo = false; // Marcamos la nueva ventana como no activa
             }
         }
+        pos_mouse = sf::Mouse::getPosition(nivel3Ventana);
+        mouse_coord = nivel3Ventana.mapPixelToCoords(pos_mouse);
+
+
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+            if (cerrarNivel3->getGlobalBounds().contains(mouse_coord)) {
+                std::cout << "Nivel 3 cerrado" << '\n';
+                nivel3Ventana.close();
+                nivel3Activo = false;
+            }
+        }
     }
 }
 
 void NivelNivel3::dibujar(sf::RenderWindow& ventana) {
-    std::cout << "Se esta dibujando el nivel 3" << std::endl;
-    //L骻ica de dibujar para el nivel 3
+
+    //L贸gica de dibujar para el nivel 3
     if (nivel3Activo) {
-        nivel3Ventana.clear(sf::Color::White); // Limpia la ventana del nivel 3 con un color blanco
-        // dibujar 
+        pos_mouse = { 0,0 };
+
+        nivel3Ventana.clear();
+        imagen->loadFromFile("./nivel3_fondo.png");
+        fondo->setTexture(*imagen);
+        nivel3Ventana.draw(*fondo);
+        cerrarNivel3->setSize(sf::Vector2f(23, 26));
+        cerrarNivel3->setPosition(1178, 39);
+        cerrarNivel3->setFillColor(sf::Color::Transparent);
+        nivel3Ventana.draw(*cerrarNivel3); // Dibujamos el rect谩ngulo cerrarVentana
+
         nivel3Ventana.display(); // Mostramos lo que hemos dibujado en la nueva ventana
     }
 }
 
 bool NivelNivel3::estaActivo() const {
-    return nivel3Activo; // Devolvemos el estado de la nueva ventana del nivel 3
+    return nivel3Activo; // Devolvemos el estado de la nueva ventana del nivel 1
+}
+
+NivelNivel3::~NivelNivel3() {
+    delete imagen;
+    delete fondo;
+    delete cerrarNivel3;
 }
