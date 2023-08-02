c73b93fa2b5a27f48b5c0bbc626e105181ce91a0
leocsip09 <131828922+leocsip09@users.noreply.github.com>
2023-08-02 06:22:56 -0500

    Update NivelNivel2.cpp

--- NivelNivel2.cpp
+++ NivelNivel2.cpp
@@ -1,17 +1,22 @@
 #include "NivelNivel2.h"
 #include <iostream>
 
-NivelNivel2::NivelNivel2() : Nivel(), nivel2Ventana(sf::VideoMode(1280, 720), "Nivel 2"), nivel2Activo(true) {}
-
-
-void NivelNivel2::inicializar() {
-    // L骻ica de inicializaci髇 para el nivel 2
+NivelNivel2::NivelNivel2() : Nivel(), nivel2Ventana(sf::VideoMode(1280, 720), "Nivel 2"), nivel2Activo(true) {
+    imagen = new sf::Texture();
+    fondo = new sf::Sprite();
+    cerrarNivel2 = new sf::RectangleShape();
     std::cout << "Se inializo el nivel 2 desde multiton" << std::endl;
 }
 
+
+void NivelNivel2::inicializar() {
+    // L贸gica de inicializaci贸n para el nivel 2
+
+}
+
 void NivelNivel2::actualizar(sf::RenderWindow& ventana) {
-    // L骻ica de actualizaci髇 para el nivel 2
-    std::cout << "Se esta actualizando el nivel 2" << std::endl;
+    // L贸gica de actualizaci贸n para el nivel 2
+
     if (nivel2Activo) {
         sf::Event event;
         while (nivel2Ventana.pollEvent(event)) {
@@ -20,20 +25,45 @@ void NivelNivel2::actualizar(sf::RenderWindow& ventana) {
                 nivel2Activo = false; // Marcamos la nueva ventana como no activa
             }
         }
+        pos_mouse = sf::Mouse::getPosition(nivel2Ventana);
+        mouse_coord = nivel2Ventana.mapPixelToCoords(pos_mouse);
+
+
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+            if (cerrarNivel2->getGlobalBounds().contains(mouse_coord)) {
+                std::cout << "Nivel 2 cerrado" << '\n';
+                nivel2Ventana.close();
+                nivel2Activo = false;
+            }
+        }
     }
 }
 
 void NivelNivel2::dibujar(sf::RenderWindow& ventana) {
-    std::cout << "Se esta dibujando el nivel 2" << std::endl;
-    //L骻ica de dibujar para el nivel 2
+
+    //L贸gica de dibujar para el nivel 2
     if (nivel2Activo) {
-        nivel2Ventana.clear(sf::Color::White); // Limpia la ventana del nivel 2 con un color blanco
-        // dibujar 
-        // ...
+        pos_mouse = { 0,0 };
+
+        nivel2Ventana.clear();
+        imagen->loadFromFile("./nivel2_fondo.png");
+        fondo->setTexture(*imagen);
+        nivel2Ventana.draw(*fondo);
+        cerrarNivel2->setSize(sf::Vector2f(23, 26));
+        cerrarNivel2->setPosition(1178, 39);
+        cerrarNivel2->setFillColor(sf::Color::Transparent);
+        nivel2Ventana.draw(*cerrarNivel2); // Dibujamos el rect谩ngulo cerrarVentana
+
         nivel2Ventana.display(); // Mostramos lo que hemos dibujado en la nueva ventana
     }
 }
 
 bool NivelNivel2::estaActivo() const {
-    return nivel2Activo; // Devolvemos el estado de la nueva ventana del nivel 2
+    return nivel2Activo; // Devolvemos el estado de la nueva ventana del nivel 1
+}
+
+NivelNivel2::~NivelNivel2() {
+    delete imagen;
+    delete fondo;
+    delete cerrarNivel2;
 }
